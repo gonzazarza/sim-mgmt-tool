51c6e24ca2cf7e7333eae90219eb7fb44be7ab8c
gazarza@gmail.com <gazarza@gmail.com@96209704-63e1-ccd6-9dcb-9a83b7d9080c>
2013-02-15 14:52:45 +0000

    New tree structure 4

---
+++ src/main/java/sim/mgmt/tool/SimMgmtTool/m1kernel/exceptions/OpnetHeavyException.java
@@ -0,0 +1,84 @@
+package sim.mgmt.tool.SimMgmtTool.m1kernel.exceptions;
+
+/* 
+ * Copyright (c) 2010-2013 Gonzalo Zarza. All rights reserved.
+ * Copyright (c) 2010-2011 Departament d'Arquitectura de Computadors i Sistemes Operatius, Universitat Autonoma de Barcelona. All rights reserved. 
+ *
+ * This file is part of the Simulation Management Tool (sim-mgmt-tool).
+ *
+ * Simulation Management Tool (sim-mgmt-tool) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Simulation Management Tool (sim-mgmt-tool) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Simulation Management Tool (sim-mgmt-tool). If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/** 
+ * Opnet project heavy exception
+ * 
+ * @author 		<a href = "mailto:gazarza@gmail.com"> Gonzalo Zarza </a>
+ * @version		2010.1026
+ */
+public class OpnetHeavyException extends OpnetExceptionClass {
+
+	/*	
+	================================================================================================================== 
+	Attributes																										
+	==================================================================================================================
+	*/
+	private static final long 				serialVersionUID 		= 1L;					//default serial version
+	private String							message					= "unknown";			//exception message
+	private String							title					= "";					//exception title
+	private int								type					= 0;					//exception type
+	
+	
+	/*	
+	================================================================================================================== 
+	Constructor																										
+	==================================================================================================================
+	*/
+	/**
+	 * Class constructor
+	 * 
+	 * @param		pException			the exception message
+	 */
+	public OpnetHeavyException(String pException){
+		
+		//call superclass constructor
+		super(pException);
+		//save exception message
+		this.message		= pException;
+		//set the dedault message type
+		this.type			= OpnetExceptionClass.MSG_TYPE_ERROR;
+		
+		
+	} // End constructor
+
+	/*	
+	================================================================================================================== 
+	Getters and Setters																										
+	==================================================================================================================
+	*/
+	/** @return the message */
+	@Override
+	public String getMessage() { return message; }
+
+	
+	/** @return the title */
+	public String getTitle(){ return title; }
+	
+	
+	/** @return the type */ 
+	@Override
+	public int getType() { return type; }	
+	
+		
+} // End OpnetHeavyException
